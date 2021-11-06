8d8f90bf8b65ca47300557091a2556c66800ca2c
nikiedev <igor.nikiforov@ukr.net>
2021-11-06 19:51:36 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function ($) {
+	$('.testimonials').slick({
+		prevArrow: '<i class="fas fa-arrow-circle-left icon-left"></i>',
+		nextArrow: '<i class="fas fa-arrow-circle-right icon-right"></i>'
+	});
+});
+
